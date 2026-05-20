34e88cbbb7e45f5663b8d731f8a0a4d47b99888c
Mamillapalli Abhiram <abhirammamillapalli173@gmail.com>
2026-05-20 23:03:36 +0530

    Fix database and storage permissions on Render

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 echo "Running database migrations and seeders..."
 php artisan migrate:fresh --seed --force
+
+echo "Setting correct folder permissions for the web server..."
+chown -R www-data:www-data /var/www/html/database /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/database /var/www/html/storage /var/www/html/bootstrap/cache
